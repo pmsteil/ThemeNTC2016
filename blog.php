846b85672464df0899da2411f2f425ca48d9f4ff
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-02 16:19:45 +0530

    added bolg temp

--- blog.php
+++ blog.php
@@ -7,24 +7,13 @@ get_header();
 <?php
   // Start the loop.
   while ( have_posts() ) : the_post();
-   // Include the page content template.
+   the_content();
   ?>
-  <style>
-  .page_default_ct{
-	  padding:30px 25px;
-/*	  background:#ddd;*/
-	  margin-bottom:40px;
-	  margin-top:40px;
-	  
-  }
-  </style>
-  <div class="row page_default_ct">
-   <?php the_content(); ?>
-      
-  </div>
+  
 <?php
    // End of the loop.
   endwhile;
   ?>
-<?php //get_sidebar(); ?>
+<?php get_sidebar('sidebarmain'); ?>
+
 <?php get_footer();?>
