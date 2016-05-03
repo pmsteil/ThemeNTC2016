2db973517ebf403f6f348c9822d1e2156784036b
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-03 15:20:57 +0530

    Updated single.php !!!

--- single.php
+++ single.php
@@ -29,8 +29,9 @@ get_header(); ?>
 			 * use this in a child theme, then include a file called called content-___.php
 			 * (where ___ is the post format) and that will be used instead.
 			 */
+                        the_content();
 			get_template_part( 'content', get_post_format() );
-
+                       
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
