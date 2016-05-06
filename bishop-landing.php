ec7eb7fc9693a90e34b6e23f9b77f352f9dc9b1b
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-06 17:46:24 +0530

    updated Bishop Landing template

--- bishop-landing.php
+++ bishop-landing.php
@@ -4,7 +4,21 @@
 */
 ?>
 <?php get_header('stripe'); ?>
-
+<style>
+.stripe_heder_wrapper h1 {
+    color: #e2ccab;
+    font-size: 32px;
+    text-align: center;
+}
+.stripe_heder_wrapper {
+    background: #003366;
+    padding-top: 31px;
+    padding-bottom: 31px;
+}
+.stripe_footer_wrapper {
+    background: #003366;
+}
+</style>
 
  <?php if ( have_posts() ) :
 	  while ( have_posts() ) : the_post(); 
@@ -13,7 +27,7 @@
 <main class="main">
     <div class="main-content-section">
         <div class="row">
-        <section id="main" class="secondary">
+        <section id="main" class="secondary1">
        <?php the_content(); ?>
        </section>
         </div>
