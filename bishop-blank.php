979ddd3c8aadc1d2d9ab736ff8fbb41fc59b2080
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-11 12:08:14 +0530

    Updated bishop-blank.php

--- bishop-blank.php
+++ bishop-blank.php
@@ -55,7 +55,20 @@
     <div class="main-content-section">
         <div class="row">
         <section id="main" class="secondary">
-       <?php the_content(); ?>
+             <article class="large-8 medium-7 index_article left-sidebar-ct columns">
+                <?php the_content(); ?>
+            </article>
+            <aside class="large-4 medium-5 columns right-sidebar-ct">
+          <?php //get_sidebar( 'sidebarmain' ) ?>
+          <sidebar id="sidebarmain" name="Main" description="Main sidebar that appears on the default page template">
+            <div id="sidecontent" class="right-sidebar-inner-ct medium-side-top">
+              <div class="sidebox">
+                <?php get_sidebar( 'sidebarmain' ) ?>
+              </div>
+            </div>
+           
+          </sidebar>
+        </aside>
        </section>
         </div>
     </div>
