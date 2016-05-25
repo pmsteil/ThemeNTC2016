99c62cc775aeb461d70a9973c24fe2b32141daae
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-25 17:32:31 +0530

    Updated header.php

--- header.php
+++ header.php
@@ -105,16 +105,17 @@
 	    ?>
      
       <div class="dropdown_ct">
-        <ul class="left dropdown_ct_list">
-        <?php if ( is_active_sidebar( 'c-dropdowns' ) ) : 
-				 dynamic_sidebar( 'c-dropdowns' ); 
-        endif; ?>
-		</ul>
+        
 		<ul class="left dropdown_ct_list"> <?php 
 		 if ( is_active_sidebar( 'd-dropdowns' ) ) : 
 				 dynamic_sidebar( 'd-dropdowns' ); 
           endif; ?>
         </ul>
+        <ul class="left dropdown_ct_list">
+        <?php if ( is_active_sidebar( 'c-dropdowns' ) ) : 
+				 dynamic_sidebar( 'c-dropdowns' ); 
+        endif; ?>
+		</ul>
       </div>
     </section>
   </nav>
