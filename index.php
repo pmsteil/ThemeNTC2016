3d4768ab51c9618001d1168a4a60398e2ab457dd
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-27 16:52:34 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -5,96 +5,70 @@
 <main class="main">
   <div class="main-content-section">
     <div class="row">
-      <section id="main" class="secondary">
+      <section id="main" class="secondary"> 
         <!-- left column -->
         <article class="large-8 medium-7 index_article left-sidebar-ct columns">
           <div id="maincontent">
-            
-        	<div id="District-Page">
-        <div class="box">
-        <div class="title title_index">
-        <h2>News Around the North Texas Conference</h2>
-        </div>
-        <div class="box-holder">
-        <div class="news-row">
-        <!--Add blog here -->
-        
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1432883"><span class="blog-list-title-only"><a href="#" title="2016 Course of Study School">New Church Development Hosting a “Passing the Baton” Workshop: May 3-4, 2016</a></span>
-      <div id="postText1432883"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/The-Change-Over-Zone-198x300.jpg"> Passing the Baton Workshop May 3, 2016, 1:00 p.m.-5:00 p.m. May 4, 2016, 9:00 a.m.-4:00 p.m. North Texas Conference Center 500 Maplelawn Dr. Plano, TX 75075 Free for North Texas Conference Staff Registration an...<a class="blog-read-more" href="../board-of-ministry/2016-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
+            <slickslider category="home"><img class="front-page-slider-margin" src="<?php echo get_template_directory_uri() ?>/images/front-page-slider1.PNG"/></slickslider>
+             <?php echo do_shortcode('[wonderplugin_carousel id="1"]'); ?>
+            <div id="District-Page">
+              <div class="box">
+                <div class="title title_index">
+                  <h2>News Around the North Texas Conference</h2>
+                </div>
+                <div class="box-holder">
+                  <div class="news-row"> 
+                    <!--Add blog here -->
+                     <?php
+       $args = array( 'category'=>219,'post_type' => 'news','posts_per_page' => 40, 'order'=> 'ASC', 'orderby' => 'id' );
+                    $postslist = get_posts( $args );
+                    foreach ( $postslist as $post ) :
+                    setup_postdata( $post ); ?> 
        
-    </li>
-  </div>
-</div>
-
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1017156"><span class="blog-list-title-only"><a href="../board-of-ministry/2014-15-course-of-study-school.html" title="2015 Course of Study School">Free Children and Youth Ministry Volunteer Training: August 13, 2016</a></span>
-      <div id="postText1017156"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/we-love-our-volunteers.jpg"> Free Volunteer Training for Children and Youth Ministries August 13, 2016 9:00 a.m.-12:00 p.m. Christ UMC Farmers Branch 2807 Valwood Pkwy. Farmers Branch, TX 75234 The Center for Leadership Development invites vol... <a class="blog-read-more" href="../board-of-ministry/2014-15-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-      
-    </li>
-  </div>
-</div>
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1017156"><span class="blog-list-title-only"><a href="../board-of-ministry/2014-15-course-of-study-school.html" title="2015 Course of Study School">April 2016 Leadership Webinars</a></span>
-      <div id="postText1017156"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/UpcomingWebinars2016-Img.JPG"> Free Leadership Development Webinars Transitions: Our Pastor Is Moving - Next Steps! Thursday, April 7, 2016 7:00 p.m. Central Daylight Time Presenter: Jacqui King, Director of Leadership for Congregational ... <a class="blog-read-more" href="../board-of-ministry/2014-15-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-      
-    </li>
-  </div>
-</div>
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1017156"><span class="blog-list-title-only"><a href="../board-of-ministry/2014-15-course-of-study-school.html" title="2015 Course of Study School">Know Your Number: Enneagram Workshop for Clergy and Lay Staff: May 26, 2016</a></span>
-      <div id="postText1017156"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/enneagram.jpg"> Know Your Number: Enneagram for Clergy and Lay Staff Thursday, May 26, 2016 8:30 a.m.-5:00 p.m. Prothro Center at Lake Texoma 269 Methodist Lane Pottsboro, TX 75076 Register Now The Enneagram is an ancient person...  <a class="blog-read-more" href="../board-of-ministry/2014-15-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-      
-    </li>
-  </div>
-</div>
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1432883"><span class="blog-list-title-only"><a href="#" title="2016 Course of Study School">New Church Development Hosting a “Passing the Baton” Workshop: May 3-4, 2016</a></span>
-      <div id="postText1432883"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/The-Change-Over-Zone-198x300.jpg"> Passing the Baton Workshop May 3, 2016, 1:00 p.m.-5:00 p.m. May 4, 2016, 9:00 a.m.-4:00 p.m. North Texas Conference Center 500 Maplelawn Dr. Plano, TX 75075 Free for North Texas Conference Staff Registration an...<a class="blog-read-more" href="../board-of-ministry/2016-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-       
-    </li>
-  </div>
-</div>
-
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1017156"><span class="blog-list-title-only"><a href="../board-of-ministry/2014-15-course-of-study-school.html" title="2015 Course of Study School">Free Children and Youth Ministry Volunteer Training: August 13, 2016</a></span>
-      <div id="postText1017156"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/we-love-our-volunteers.jpg"> Free Volunteer Training for Children and Youth Ministries August 13, 2016 9:00 a.m.-12:00 p.m. Christ UMC Farmers Branch 2807 Valwood Pkwy. Farmers Branch, TX 75234 The Center for Leadership Development invites vol... <a class="blog-read-more" href="../board-of-ministry/2014-15-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-      
-    </li>
-  </div>
-</div>
-
-<div id="blogbackground">
-  <div class="post-body">
-    <li class="blog-list-title" id="postnum1017156"><span class="blog-list-title-only"><a href="../board-of-ministry/2014-15-course-of-study-school.html" title="2015 Course of Study School">April 2016 Leadership Webinars</a></span>
-      <div id="postText1017156"><img class="blog-thumbnail" src="<?php echo get_template_directory_uri() ?>/images/UpcomingWebinars2016-Img.JPG"> Free Leadership Development Webinars Transitions: Our Pastor Is Moving - Next Steps! Thursday, April 7, 2016 7:00 p.m. Central Daylight Time Presenter: Jacqui King, Director of Leadership for Congregational ... <a class="blog-read-more" href="../board-of-ministry/2014-15-course-of-study-school.html">read&nbsp;more&nbsp;»</a><br></div>
-      
-    </li>
-  </div>
-</div>
-
-        </div>
-        <div style="text-align: right; font-weight: bold;"><a href="../board-of-ministry.html" style="font-weight: bold;"></a><a href="../board-of-ministry.html">More News and Events</a></div>
-        </div>
-        </div>
-        </div>
+                          <div id="blogbackground">
+                               <div class="post-body">
+                                 <li class="blog-list-title" id="postnum1432883">
+                                    <span class="blog-list-title-only">
+                                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                                    </span>
+                                   <div id="postText1432883">
+                                    <div class="blog-thumbnail"><?php the_post_thumbnail(); ?></div>
+                                    <?php the_excerpt(); ?>
+                                    <a class="blog-read-more" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">read&nbsp;more&nbsp;»</a>
+                                   </div>
+                                 </li>
+                                </div>
+                            </div>
+       <?php
+                    endforeach; 
+                    wp_reset_postdata();
+                    ?>    
+                    
+                 
+                  </div>
+                  <div style="text-align: right; font-weight: bold;"><a href="../board-of-ministry.html" style="font-weight: bold;"></a><a href="../board-of-ministry.html">More News and Events</a></div>
+                </div>
+              </div>
+            </div>
           </div>
+          <div class="footer_media"> <a class="readmore" href="/_blog/North_Texas_Conference_News">Read more News Around The Conference »</a><br>
+            <a href="/subscribe" class="button" title="Subscribe to The North Texas Connection email news" style="margin-top: 5px; margin-right: 20px; margin-bottom: 10px;">Subscribe</a> <a href="/stories" class="button" title="Submit a story" style="margin-top: 5px; margin-bottom: 10px;">Submit a Story</a> <br>
+            Subscribe to the RSS Feed: <a target="_blank" href="images/icon-rss.png?vs=b2004.r486545-phase1" id="rss" catid="58,12423" sw="1"><img style="border-width: 0px; border-style: solid;" src="<?php echo get_template_directory_uri() ?>/images/icon-rss.png?vs=b2004.r486545-phase1" alt="RSS" height="16" width="16"></a> </div>
+          <br>
+          <br>
         </article>
-        <!-- right column -->
+        <!-- right column --> 
         
         <aside class="large-4 medium-5 columns right-sidebar-ct">
-        <?php get_sidebar( 'sidebarmain' ) ?>
+          <?php //get_sidebar( 'sidebarmain' ) ?>
+          <sidebar id="sidebarmain" name="Main" description="Main sidebar that appears on the default page template">
+            <div id="sidecontent" class="right-sidebar-inner-ct medium-side-top">
+              <div class="sidebox">
+                <?php get_sidebar( 'sidebarmain' ) ?>
+              </div>
+            </div>
+           
+          </sidebar>
         </aside>
       </section>
     </div>
