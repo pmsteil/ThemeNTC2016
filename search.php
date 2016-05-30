a873fc728dc9894bf7180d8f23b2286c77fceca1
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-30 13:24:31 +0530

    search.php

--- search.php
+++ search.php
@@ -1,82 +1,31 @@
-<?php
-/*
-    Template Name: search
-*/
-?>
 <?php get_header(); ?>
-<!--========main content section start==============-->
 
-<main class="main">
-  <div class="main-content-section">
-    <div class="row">
-      <section id="main" class="secondary">
-        <!-- left column -->
-        <article class="large-8 medium-7 left-sidebar-ct columns">
-          <div id="maincontent">
-            <h1>Site Search</h1>
-            	 <?php get_search_form() ?>
-            
-             <searchresults>
-                  <h1 class="content-title">Search Results for "<?php echo get_search_query(); ?>"</h1>
+<!-- Row for main content area -->
+	<div class="small-12 large-8 columns" id="content" role="main">
+	
+		<h2><?php _e('Search Results for', 'reverie'); ?> "<?php echo get_search_query(); ?>"</h2>
+	
+	<?php if ( have_posts() ) : ?>
+	
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
+		
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		
+	<?php endif; // end have_posts() check ?>
+	
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('reverie_pagination') ) { reverie_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'reverie' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'reverie' ) ); ?></div>
+		</nav>
+	<?php } ?>
 
-                  <?php
-                  $args = array( 'paged'=> $paged,
-                                 'post_type' => 'post');
-                  $more = 0;
-                  query_posts($args);
-                  $image_url = $site_options['blog_image'];
-                  ?>
-                  <?php if (have_posts()) : $count = 0; ?>
-                  <?php while (have_posts()) : the_post(); $count++; ?>
-                      <?php
-                      $ImageID = get_post_thumbnail_id($post->ID);
-                      if($ImageID)
-                         // get the image url
-                         $image_url = wp_get_attachment_url($ImageID);
-                      $excerpt = get_the_excerpt();
-                      $excerpt = preg_replace('/\s+?(\S+)?$/', '', $excerpt);
-                      ?>
-                        <div class="recent-box clearfix">
-                          <div class="recent-box-img">
-                            <img src="<?php echo $image_url; ?>" alt="">
-                          </div>
-                          <div class="recent-box-text">
-                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                            <p>
-                              <?php echo $excerpt; ?>
-                              <a href="<?php the_permalink(); ?>">Read More</a>
-                            </p>
-                          </div>
-                        </div>
-                  <?php endwhile; else: ?>
-                    <div class="post">
-                        <p>Sorry, no posts matched your criteria.</p>
-                    </div><!-- /.post -->
-                  <?php endif; ?>
-
-                  <?php
-                    if(function_exists('wp_pagenavi')) {
-                      wp_pagenavi();
-                    }
-                    else {
-                      $next_page_link = next_page_not_post('');
-                      if (function_exists('apply_all_short_codes'))
-                         $next_page_link =  apply_all_short_codes($next_page_link);
-                      echo '<div style="clear:both">' . $next_page_link . '</div>';
-                    }
-                  ?></searchresults>
-          
-          </div>
-        </article>
-        <!-- right column -->
-        <aside class="large-4 medium-5 columns right-sidebar-ct">
-        <?php get_sidebar( 'sidebarmain' ) ?>
-        </aside>
-      </section>
-    </div>
-  </div>
-</main>
-
-<!--========main content section start==============-->
-<!--===Footer section starts===-->
+	</div>
+	<?php get_sidebar(); ?>
+		
 <?php get_footer(); ?>
