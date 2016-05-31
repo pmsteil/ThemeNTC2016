f1e233c82db2e3c6799175734328ba963499008b
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-31 12:46:00 +0530

    updated front-page.php

--- front-page.php
+++ front-page.php
@@ -7,11 +7,14 @@
 
 <!--========main content section start==============-->
 <?php
-
+/*
 if ( 'posts' == get_option( 'show_on_front' ) ) {
-    //include( get_home_template() );
-	?>
-	<main class="main">
+    include( get_home_template() );
+} else {
+    include( get_page_template() );
+} */ 
+ ?>
+<main class="main">
   <div class="main-content-section">
     <div class="row">
       <section id="main" class="secondary"> 
@@ -83,12 +86,6 @@ if ( 'posts' == get_option( 'show_on_front' ) ) {
     </div>
   </div>
 </main>
-<?php
-} else {
-    include( get_page_template() );
-} 
- ?>
-
 
 <!--========main content section start==============--> 
 
