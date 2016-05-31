d6edec7aa0b628718f694440e2bfd3e775fedda5
Mukul Misra <er.mukulmisra@gmail.com>
2016-05-31 15:10:09 +0530

    Updated ntc.php

--- ntc.php
+++ ntc.php
@@ -18,51 +18,11 @@
         <!-- left column -->
         <article class="large-8 medium-7 index_article left-sidebar-ct columns">
           <div id="maincontent">
-            <slickslider category="home"><img class="front-page-slider-margin" src="http://dev.northtexasumc.org/wp-content/themes/ThemeNTC2016/images/front-page-slider1.PNG"/></slickslider>
-             <?php echo do_shortcode('[wonderplugin_carousel id="1"]'); ?>
             <div id="District-Page">
-              <div class="box">
-                <div class="title title_index">
-                  <h2>News Around the North Texas Conference</h2>
-                </div>
-                <div class="box-holder">
-                  <div class="news-row"> 
-                    <!--Add blog here -->
-                     <?php
-       $args = array( 'category'=>219,'post_type' => 'news','posts_per_page' => 40, 'order'=> 'ASC', 'orderby' => 'id' );
-                    $postslist = get_posts( $args );
-                    foreach ( $postslist as $post ) :
-                    setup_postdata( $post ); ?> 
-       
-                          <div id="blogbackground">
-                               <div class="post-body">
-                                 <li class="blog-list-title" id="postnum1432883">
-                                    <span class="blog-list-title-only">
-                                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                                    </span>
-                                   <div id="postText1432883">
-                                    <div class="blog-thumbnail"><?php the_post_thumbnail(); ?></div>
-                                    <?php the_excerpt(); ?>
-                                    <a class="blog-read-more" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">read&nbsp;more&nbsp;»</a>
-                                   </div>
-                                 </li>
-                                </div>
-                            </div>
-       <?php
-                    endforeach; 
-                    wp_reset_postdata();
-                    ?>    
-                    
-                 
-                  </div>
-                  <div style="text-align: right; font-weight: bold;"><a href="../board-of-ministry.html" style="font-weight: bold;"></a><a href="../board-of-ministry.html">More News and Events</a></div>
-                </div>
-              </div>
+              <?php the_content(); ?>
             </div>
           </div>
-          <div class="footer_media"> <a class="readmore" href="/_blog/North_Texas_Conference_News">Read more News Around The Conference »</a><br>
-            <a href="/subscribe" class="button" title="Subscribe to The North Texas Connection email news" style="margin-top: 5px; margin-right: 20px; margin-bottom: 10px;">Subscribe</a> <a href="/stories" class="button" title="Submit a story" style="margin-top: 5px; margin-bottom: 10px;">Submit a Story</a> <br>
-            Subscribe to the RSS Feed: <a target="_blank" href="images/icon-rss.png?vs=b2004.r486545-phase1" id="rss" catid="58,12423" sw="1"><img style="border-width: 0px; border-style: solid;" src="http://dev.northtexasumc.org/wp-content/themes/ThemeNTC2016/images/icon-rss.png?vs=b2004.r486545-phase1" alt="RSS" height="16" width="16"></a> </div>
+          
           <br>
           <br>
         </article>
