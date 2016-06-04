f59eddc91bb2aa206c2c072c52866d70f3583d8c
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-04 08:20:12 +0530

    Updated Category.php

--- category.php
+++ category.php
@@ -55,7 +55,7 @@ h1.single_post_title.single_post_title12 {
         <article class="large-8 medium-7 left-sidebar-ct columns">
         <?php if ( have_posts() ) : ?>
 			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentythirteen' ), single_cat_title( '', false ) ); ?></h1>
+				<h1 class="archive-title"><?php printf( __( '%s', 'twentythirteen' ), single_cat_title( '', false ) ); ?></h1>
 
 				<?php if ( category_description() ) : // Show an optional category description ?>
 				<div class="archive-meta"><?php echo category_description(); ?></div>
