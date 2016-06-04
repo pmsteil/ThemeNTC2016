69f6eb008eae11c2308df21e5b61d1a61c4c3b55
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-04 09:56:19 +0530

    Updated category page

--- category.php
+++ category.php
@@ -65,9 +65,9 @@ h1.single_post_title.single_post_title12 {
 				 $cat_id =  get_cat_id( single_cat_title("",false) );exit;
 				 do_shortcode('[ntclistcategorypost category="'.$cat_id.'" order_by_post="DESC" per_page="10" pagerange="2" post_type="post" maxlimit="200"]'); ?>		
 			<?php /* The loop */ ?>
-			<?php /* while ( have_posts() ) : the_post(); ?>
+			<?php  while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; */ ?>
+			<?php endwhile; ?>
 
 			<?php //twentythirteen_paging_nav(); ?>
 
