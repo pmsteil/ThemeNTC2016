2027028aee220cfabc46d4487db7b9639dd366cb
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-06 12:17:36 +0530

    Updated category.php

--- category.php
+++ category.php
@@ -62,10 +62,10 @@ h1.single_post_title.single_post_title12 {
 				<?php endif; ?>
 			</header><!-- .archive-header -->
 				<?php
-				 echo $cat_id =  get_cat_id( single_cat_title("",false) );
+				 $cat_id =  get_cat_id( single_cat_title("",false) );
 				 ?>		
 			<?php /* The loop */
-			echo do_shortcode('[ntclistcategorypost category="'.$cat_id.'" order_by_post="DESC" per_page="10" pagerange="2" post_type="post" maxlimit="200"]');	
+			
 			 ?>
             
 			<?php while ( have_posts() ) : the_post(); ?>
