4d4f2fe4626917e1f9d50ef8ae1b72272cd79342
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-07 15:50:33 +0530

    Updated functions.php

--- functions.php
+++ functions.php
@@ -1242,7 +1242,6 @@ function ntc_list_category_post($atts) {
                    if ( $postslist->have_posts() ) :
 				    $i=1;
 				   ?>
-                   <div style="clear:both"> </div>
                  <div class="box">
                 <div class="title title_index">
                   <?php if(!empty($top_stripe_title)){	?>
