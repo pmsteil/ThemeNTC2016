bd568edd3853a4fa9fd13b7f59e288010d51a578
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-23 15:50:16 +0530

    Updated churches and district Template

--- for-churches-menu.php
+++ for-churches-menu.php
@@ -3,6 +3,12 @@
 	 Template Name: For Churches Menu
 */
  get_header();?>
+ <div class="breadcrumbs breadcrumbs_custom" typeof="BreadcrumbList" vocab="">
+						  <?php if(function_exists('bcn_display'))
+                            { 
+                            bcn_display();
+                            }?>
+                    </div>
  
    <?php if ( have_posts() ) :
 	  while ( have_posts() ) : the_post(); 
--- for-district.php
+++ for-district.php
@@ -8,6 +8,12 @@ get_header();
                   <div class="districts-popup">
                     <div class="head large-12">
                       <div class="churches-options">
+                      <div class="breadcrumbs breadcrumbs_custom" typeof="BreadcrumbList" vocab="">
+						  <?php if(function_exists('bcn_display'))
+                            { 
+                            bcn_display();
+                            }?>
+                    </div>
 
    <?php if ( have_posts() ) :
 	  while ( have_posts() ) : the_post(); 
