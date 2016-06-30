f239389e3adabf71553a361bdae7e30ea1ff2f8f
Mukul Misra <er.mukulmisra@gmail.com>
2016-06-30 19:54:49 +0530

    delete laity-theme folder

--- laity theme/Laity/Laity/searchform.php
+++
@@ -1,6 +0,0 @@
-
-                  <form role="search" name="search" class="form-search" method="get" action="/">
-                    <input type="text" name="CAT_Search" title="site search" class="txt" id="CAT_Search" accesskey="4">
-                    <input type="submit" class="btn-submit cat_button" value="Search">
-                  </form>
-                
